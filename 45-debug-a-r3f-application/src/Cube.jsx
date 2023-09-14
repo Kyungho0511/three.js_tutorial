5b7eae11b9ca8b41cd6bfddcd029b969d357a304
Kyungho0511 <kleearchitecture@gmail.com>
2023-09-14 14:04:57 -0400

    Add 45-debug-r3f-application

---
+++ 45-debug-a-r3f-application/src/Cube.jsx
@@ -0,0 +1,10 @@
+export default function Cube({ scale = 1 }) {
+  return (
+    <>
+      <mesh position-x={2} scale={scale}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+    </>
+  );
+}
